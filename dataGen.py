c255edb637689d19e8ca864307fbf4305058b31d
dptru10 <dptru10@gmail.com>
2021-07-19 15:21:05 -0400

    Added dataGen.py for generating simulated data

---
+++ dataGen.py
@@ -0,0 +1,11 @@
+from t_gauss import genTrainData, genValData
+
+train_split = 0.9 
+num_images  = 2000
+train_img   = int(num_images * train_split) 
+val_img     = int(num_images * (1.0 - train_split))
+
+genTrainData(train_img)
+
+genValData(val_img)
+
