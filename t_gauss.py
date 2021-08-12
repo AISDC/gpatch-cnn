c3e8683ea786a8905d8385e77cc91cbb6f556051
Dennis Trujillo <dennis@dft-oxides.uconn.edu>
2021-08-12 15:21:50 -0600

    added fixed to patchgen demo and t_gauss lib

--- t_gauss.py
+++ t_gauss.py
@@ -16,8 +16,7 @@ from torch.optim import lr_scheduler
 from torchvision import datasets, models, transforms
 from PIL import Image as im
 
-def singleGauss(amp,sigma_x,sigma_y,x0,y0):
-    size = 2048
+def singleGauss(size,amp,sigma_x,sigma_y,x0,y0):
     x    = torch.linspace(-1, 1, size)
     y    = torch.linspace(-1, 1, size)
     x, y = torch.meshgrid(x, y)
@@ -26,7 +25,7 @@ def singleGauss(amp,sigma_x,sigma_y,x0,y0):
     return z
 
 def AmpRand():
-    return 1 #random.random()
+    return random.random()
 
 def SigmaRand():
     return random.random()*0.5
@@ -34,11 +33,11 @@ def SigmaRand():
 def initRand():
     return random.random()*0.5
 
-def multiGauss(n):
-    size = 2048
+def multiGauss(n,size):
+    size = 11
     z = torch.zeros(size=(size,size))
     for i in range(n):
-        z_i   = singleGauss(AmpRand(),SigmaRand(),SigmaRand(),initRand(),initRand())
+        z_i   = singleGauss(size,AmpRand(),SigmaRand(),SigmaRand(),initRand(),initRand())
         z    += z_i
     return z
 
@@ -48,29 +47,29 @@ def torchMax(z):
 def write_images(z,img_name):
     torchvision.utils.save_image(z,img_name)
 
-def genTrainData(trainSize): 
-    if os.path.exists('./data/train/two') is False:
-        os.mkdir('./data/train/two')
+def genTrainData(patchSize,trainSize): 
+    if os.path.exists('data/train/two') is False:
+        os.mkdir('data/train/two')
     for i in range(trainSize):
         test_mult=multiGauss(2)
-        write_images(test_mult,'./data/train/two/img%i.png' %i)
+        write_images(test_mult,'data/train/two/img%i.png' %i)
     
-    if os.path.exists('./data/train/one') is False:
-        os.mkdir('./data/train/one')
+    if os.path.exists('data/train/one') is False:
+        os.mkdir('data/train/one')
     for i in range(trainSize):
         test=singleGauss(SigmaRand(),SigmaRand(),initRand(),initRand(),initRand())
-        write_images(test,'./data/train/one/img%i.png' %i)
+        write_images(test,'data/train/one/img%i.png' %i)
 
-def genValData(valSize): 
-    if os.path.exists('./data/val/two') is False:
-        os.mkdir('./data/val/two')
+def genValData(patchSize,valSize): 
+    if os.path.exists('data/val/two') is False:
+        os.mkdir('data/val/two')
     for i in range(valSize):
-        test_mult=multiGauss(2)
-        write_images(test_mult,'./data/val/two/img%i.png' %i)
+        test_mult=multiGauss(patchSize,2)
+        write_images(test_mult,'data/val/two/img%i.png' %i)
     
-    if os.path.exists('./data/val/one') is False:
-        os.mkdir('./data/val/one')
+    if os.path.exists('data/val/one') is False:
+        os.mkdir('data/val/one')
     for i in range(valSize):
-        test=singleGauss(SigmaRand(),SigmaRand(),initRand(),initRand(),initRand())
+        test=singleGauss(patchSize,SigmaRand(),SigmaRand(),initRand(),initRand(),initRand())
         write_images(test,'./data/val/one/img%i.png' %i)
 
