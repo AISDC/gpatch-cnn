1c63b9864b5a4545783c76514b719da65e148301
dptru10 <dptru10@gmail.com>
2021-11-18 14:45:40 -0600

    updated dir structures

--- dataGen.py
+++
@@ -1,9 +0,0 @@
-from gpatch_cnn.torch_gauss import genData 
-
-train_split = 0.9 
-num_images  = 2000
-train_img   = int(num_images * train_split) 
-val_img     = int(num_images * (1.0 - train_split))
-
-train=genData(patch_size,train_img,n_peaks)
-val=genData(patch_size,val_img,n_peaks)
