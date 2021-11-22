88abb5bb3302b3aaf182cfb2db0d6cff0f42e0e9
Dennis Trujillo <dennis@dft-oxides.uconn.edu>
2021-11-22 09:58:56 -0700

    added dataGen.py

--- examples/dataGen.py
+++ examples/dataGen.py
@@ -1,9 +1,23 @@
-from gpatch_cnn.torch_gauss import genData 
+import os 
+from gpatch_cnn.torch_gauss import multiGauss, write_image 
 
-train_split = 0.9 
-num_images  = 2000
-train_img   = int(num_images * train_split) 
-val_img     = int(num_images * (1.0 - train_split))
+patch_size  = 16#128
+n_peaks     = 5
+train_n     = 1800
+val_n       = 200
 
-train=genData(patch_size,train_img,n_peaks)
-val=genData(patch_size,val_img,n_peaks)
+
+for n in range(1,n_peaks+1):
+    train_path = 'data/train/%i' %n 
+    os.makedirs(train_path,exist_ok=True)
+    for i in range(train_n):
+        img = multiGauss(n,patch_size)
+        write_image(img,train_path+'/img_%i.png' %i)
+
+
+for n in range(1,n_peaks+1): 
+    val_path = 'data/val/%i' %n  
+    os.makedirs(val_path,exist_ok=True)
+    for j in range(val_n): 
+        img = multiGauss(n,patch_size)
+        write_image(img,val_path+'/img_%i.png' %i)
