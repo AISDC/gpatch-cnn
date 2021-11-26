3b6d8360b3a72b8fa5d913a7bd1169ad9e9da19e
dptru10 <dptru10@gmail.com>
2021-11-26 15:34:46 -0500

    Update dataGen.py

--- examples/dataGen.py
+++ examples/dataGen.py
@@ -1,23 +1,23 @@
 import os 
-from gpatch_cnn.torch_gauss import multiGauss, write_image 
+from gpatch_cnn.torch_gauss import multiGauss, multiGaussNoOverlap, write_image 
 
-patch_size  = 16#128
+patch_size  = 256
 n_peaks     = 5
-train_n     = 1800
-val_n       = 200
+train_n     = 10
+val_n       = 2
+cutoff      = 0.2
 
-
-for n in range(1,n_peaks+1):
+for n in range(n_peaks):
     train_path = 'data/train/%i' %n 
     os.makedirs(train_path,exist_ok=True)
     for i in range(train_n):
-        img = multiGauss(n,patch_size)
+        img = multiGaussNoOverlap(n,patch_size,cutoff)
         write_image(img,train_path+'/img_%i.png' %i)
 
 
-for n in range(1,n_peaks+1): 
-    val_path = 'data/val/%i' %n  
-    os.makedirs(val_path,exist_ok=True)
-    for j in range(val_n): 
-        img = multiGauss(n,patch_size)
-        write_image(img,val_path+'/img_%i.png' %i)
+#for n in range(n_peaks): 
+#    val_path = 'data/val/%i' %n  
+#    os.makedirs(val_path,exist_ok=True)
+#    for j in range(val_n): 
+#        img = multiGaussNoOverlap(n,patch_size,cutoff)
+#        write_image(img,val_path+'/img_%i.png' %i)
