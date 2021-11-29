1c8f4a44032dda850f974562eb67665f5602a7b4
dptru10 <dptru10@gmail.com>
2021-11-28 19:47:27 -0500

    Update dataGen.py

--- examples/dataGen.py
+++ examples/dataGen.py
@@ -1,11 +1,11 @@
 import os 
 from gpatch_cnn.torch_gauss import multiGauss, multiGaussNoOverlap, write_image 
 
-patch_size  = 256
+patch_size  = 32
 n_peaks     = 5
-train_n     = 10
-val_n       = 2
-cutoff      = 0.2
+train_n     = 1000
+val_n       = 200
+cutoff      = 0.4
 
 for n in range(n_peaks):
     train_path = 'data/train/%i' %n 
@@ -15,9 +15,9 @@ for n in range(n_peaks):
         write_image(img,train_path+'/img_%i.png' %i)
 
 
-#for n in range(n_peaks): 
-#    val_path = 'data/val/%i' %n  
-#    os.makedirs(val_path,exist_ok=True)
-#    for j in range(val_n): 
-#        img = multiGaussNoOverlap(n,patch_size,cutoff)
-#        write_image(img,val_path+'/img_%i.png' %i)
+for n in range(n_peaks): 
+    val_path = 'data/val/%i' %n  
+    os.makedirs(val_path,exist_ok=True)
+    for j in range(val_n): 
+        img = multiGaussNoOverlap(n,patch_size,cutoff)
+        write_image(img,val_path+'/img_%i.png' %i)
