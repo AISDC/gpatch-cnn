9791d7d3a4d42a3e923b3e973073b5866758f81e
dptru10 <dptru10@gmail.com>
2022-01-21 14:08:10 -0600

    Update model.py

--- gpatch_cnn/model.py
+++ gpatch_cnn/model.py
@@ -16,6 +16,22 @@ from torchvision import datasets, models, transforms
 from PIL import Image as im
 
 
+class ImageFolderWithPaths(datasets.ImageFolder):
+    """Custom dataset that includes image file paths. Extends
+    torchvision.datasets.ImageFolder
+    """
+
+    # override the __getitem__ method. this is the method that dataloader calls
+    def __getitem__(self, index):
+        # this is what ImageFolder normally returns 
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+        # the image file path
+        path = self.imgs[index][0]
+        # make a new tuple that includes original and the path
+        tuple_with_path = (original_tuple + (path,))
+        return tuple_with_path
+
+
 class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
         self.std = std
